5ba59f6441ad1e108dbe2f90090e0b8ab92fabd8
axxe16 <axxe16@users.noreply.github.com>
2019-03-10 11:59:04 +0100

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -4,21 +4,27 @@ require_once 'FacebookBot.php';
 $bot = new FacebookBot(FACEBOOK_VALIDATION_TOKEN, FACEBOOK_PAGE_ACCESS_TOKEN);
 $bot->run();
 $messages = $bot->getReceivedMessages();
-
 foreach ($messages as $message)
 {
 	$recipientId = $message->senderId;
-	if($message->text)
+	if($message->text == trim("ciao"))
 	{
-		$response = processRequest($message->text);
-		$bot->sendTextMessage($recipientId, $response);
+		$bot->sendTextMessage($recipientId, $message->text . ' a te! ' . print_r($bot,true));
 	}
+	elseif($message->text) {
+		$bot->sendTextMessage($recipientId, $message->text);
+	}
+	elseif($message->attachments)
+	{
+		$bot->sendTextMessage($recipientId, "Attachment received");
+	}
+	
 }
 
-//rispondo in base al testo contenuto nel messaggio
 function processRequest($text)
 {
-	$text = trim(strtolower($text));
+	$text = trim($text);
+	$text = strtolower($text);
 	$response = "";
 	if($text=="domanda 1")
 	{
@@ -27,6 +33,10 @@ function processRequest($text)
 	elseif ($text=="domanda 2")
 	{
 		$response = "risposta alla domanda 2";
+	}
+	elseif ($text=="debug")
+	{
+		$response = print_r($request);
 	}
 	else
 	{
